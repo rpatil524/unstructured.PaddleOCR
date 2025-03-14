8520ef7c7f066418f666e12f49026fabb53c1dbb
Yao You <yao@unstructured.io>
2025-03-14 12:48:38 -0500

    sort out build system

--- unstructured_paddleocr/__init__.py
+++ unstructured_paddleocr/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .paddleocr import (
+from .unstructured_paddleocr import (
     PaddleOCR,
     PPStructure,
     draw_ocr,
